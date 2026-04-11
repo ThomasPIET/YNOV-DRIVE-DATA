895276f5131ef3a26b089d378a8e6c58995e35ee
thomas <thomaspiet@outlook.fr>
2026-04-11 10:44:10 +0200

    add checkServiceToken middleware

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { userController } from '../controllers/user.controller.js';
+import { checkServiceToken } from '../middleware/serviceToken.middleware.js';
 
 const router = Router();
 
-router.get('/me', userController.getCurrentUser);
-router.patch('/me', userController.editCurrentUser)
+router.get('/me', checkServiceToken, userController.getCurrentUser);
+router.patch('/me', checkServiceToken, userController.editCurrentUser)
 
-router.post('/', userController.createUser)
+router.post('/', checkServiceToken, userController.createUser)
 
 export default router;
